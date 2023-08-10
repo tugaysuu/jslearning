4d394fe5495fda18d319cd66816a278cca4325b8
tugaysuu <sutugay@gmail.com>
2023-08-10 15:17:06 +0300

    promisess

--- promise.js
+++ promise.js
@@ -1,11 +1,12 @@
 const myPromise = new Promise((resolve, reject) => {
-  const rand = Math.floor(Math.random() * 2);
-  if (rand === 0) {
-    resolve();
-  } else {
-    reject();
-  }
+    const rand = Math.floor(Math.random() * 2);
+    if(rand === 0){
+        resolve();
+    }else {
+        reject();
+    }
 });
 
-myPromise.then(() => console.log("success"));
-myPromise.catch(() => console.error("something is wrong"));
+myPromise
+    .then(() => console.log("success"))
+    .catch(() => console.error("something"))
