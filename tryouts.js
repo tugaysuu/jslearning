b97f2b46b6aa84b95aaff75f2d99d0433781e81c
tugaysuu <sutugay@gmail.com>
2023-08-14 15:47:45 +0300

    more video tries

--- tryouts.js
+++ tryouts.js
@@ -47,12 +47,103 @@
 
 //---------------------------------------------------------------
 
-function factorial(n) {
-    if (n === 0 || n === 1) {
-      return 1;
-    } else {
-      return n * factorial(n - 1);
-    }
+//function factorial(n) {
+//    if (n === 0 || n === 1) {
+//      return 1;
+//    } else {
+//      return n * factorial(n - 1);
+//    }
+//  }
+
+//  console.log(factorial(170))
+
+
+//---------------------------------------------------------------
+
+
+/* function getData() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(46);
+    }, 1);
+  });
+}
+
+async function start() {
+
+const result = await getData()
+console.log(result)
+
+}
+
+async function start2() {
+
+  getData()
+  .then(result => console.log(result))
+  
   }
 
-  console.log(factorial(170))
+start() */
+
+//---------------------------------------------------------------
+/* this doesnt work yet so try at home
+async function start() {
+  const data = await fetch('http://api.weatherapi.com/v1')
+  const result = await data.json()
+  console.log(result)
+}
+
+start() */
+
+//---------------------------------------------------------------
+/* 
+async function start() {
+  fetch('http://api.weatherapi.com/v1')
+    .then(data => data.json())
+    .then(result => {
+      console.log(result.properties)
+  })
+}  
+  start()
+ */
+
+  //---------------------------------------------------------------
+
+
+  /* function getData() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(() => {
+        resolve('here is your data')
+        //reject('something went wrong!')
+      }, 1)
+    })
+    }
+
+    async function start() {
+      try {
+        const result = await getData()
+      } catch (error) {
+        console.log(`ERROR: ${error}`)
+      }
+    }
+  
+    async function start2() {
+      const result = await getData()
+      console.log(result)
+       // .catch(error)
+       //   console.log(`ERROR: ${error}`)
+    }
+
+    start2() */
+
+    //---------------------------------------------------------------
+
+    const url = 'http://worldtimeapi.org/api/timezone/America/New_York'
+
+    async function getData(){
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+
+    getData()
